bc68e84495d4e917f1f755aae4fcdbb9bb0180f7
Dipak-Gautam <anjangautam095@gmail.com>
2025-07-19 20:20:17 +0545

    quantity completed

--- e-commerce/src/LocalStorage/subQuantity.js
+++ e-commerce/src/LocalStorage/subQuantity.js
@@ -1,8 +1,9 @@
-const addQuantity = (setCartData, productData) => {
+const subQuantity = (setCartData, productData) => {
+  if (productData.quantity <= 1) return;
   const localData = JSON.parse(localStorage.getItem("cart5"));
   const index = localData.findIndex((item) => item.id == productData.id);
-  localData[index].quantity += 1;
+  localData[index].quantity -= 1;
   setCartData(localData);
   localStorage.setItem("cart5", JSON.stringify(localData));
 };
-export default addQuantity;
+export default subQuantity;
