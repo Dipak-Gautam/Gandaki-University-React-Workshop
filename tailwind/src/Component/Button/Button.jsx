ca2e0b5ad9402bb98611bdc156b8135dce6bf1f9
Dipak-Gautam <anjangautam095@gmail.com>
2025-09-13 20:14:47 +0545

    feat: ui check

--- tailwind/src/Component/Button/Button.jsx
+++ tailwind/src/Component/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ data, ...props }) => {
   return (
     <div
-      className=" flex w-12 h-12 border bg-gray-600 text-white font-bold text-3xl justify-center items-center rounded-md cursor-pointer"
+      className=" flex w-12 h-12 border bg-gray-600 text-white font-bold text-3xl justify-center items-center rounded-md cursor-pointer hover:bg-gray-800"
       {...props}
     >
       {data}
